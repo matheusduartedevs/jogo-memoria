31edc70edf619fea5ccf5637f2c398eae3577084
unknown <matheus.duartesilva42@gmail.com>
2023-09-30 15:33:01 -0300

    implementação funcional dos card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,14 @@
-const Card = () => {
-  return (
-    <div className="carta">Card</div>
-  )
+const Card = ({ id, idDoPar, imagem }) => {
+    return (
+        <button id={id} className="carta">
+            <div className="carta__conteudo">
+                <div className="carta__frente"></div>
+                <div className="costas">
+                    <img src={imagem} alt={`Carta ${id}`} width={300} />
+                </div>
+            </div>
+        </button>
+    )
 }
 
 export default Card
